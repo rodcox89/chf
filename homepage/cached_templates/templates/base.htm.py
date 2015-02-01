6eb403efa124c9c7a8404cea3101c84d7534a789
rodcox89 <neddam12>
2015-02-01 00:29:37 -0700

    coming along
    coming along

--- homepage/cached_templates/templates/base.htm.py
+++ homepage/cached_templates/templates/base.htm.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1422762521.695699
+_modified_time = 1422769302.36635
 _enable_loop = True
 _template_filename = '/Users/rodneycox/chf/homepage/templates/base.htm'
 _template_uri = 'base.htm'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['content', 'footlinks', 'headlinks']
+_exports = ['content', 'headlinks', 'footlinks']
 
 
 from django_mako_plus.controller import static_files 
@@ -19,14 +19,14 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        request = context.get('request', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
-        def footlinks():
-            return render_footlinks(context._locals(__M_locals))
         self = context.get('self', UNDEFINED)
         def headlinks():
             return render_headlinks(context._locals(__M_locals))
+        request = context.get('request', UNDEFINED)
+        def footlinks():
+            return render_footlinks(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -35,7 +35,7 @@ def render_body(context,**pageargs):
         __M_locals_builtin_stored = __M_locals_builtin()
         __M_locals.update(__M_dict_builtin([(__M_key, __M_locals_builtin_stored[__M_key]) for __M_key in ['static_renderer'] if __M_key in __M_locals_builtin_stored]))
         __M_writer('\n\n<!DOCTYPE html>\n<html>\n  \n  <head>\n    <meta charset="UTF-8">\n    <title>homepage</title>\n    \n')
-        __M_writer('    <!-- Latest compiled and minified CSS -->\n    <link href=\'http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic\' rel=\'stylesheet\' type=\'text/css\'>\n    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/dark-bootstrap/all.min.css">\n    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">\n    <link rel="stylesheet" href="http://www.shieldui.com/shared/components/latest/css/shieldui-all.min.css">\n    <link rel="stylesheet" href="http://www.shieldui.com/shared/components/latest/css/light-bootstrap/all.min.css">\n    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/dark-bootstrap/all.min.css">\n    ')
+        __M_writer('    <!-- Latest compiled and minified CSS -->\n    \n    <link href=\'http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic\' rel=\'stylesheet\' type=\'text/css\'>\n    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/dark-bootstrap/all.min.css">\n    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">\n    <link rel="stylesheet" href="http://www.shieldui.com/shared/components/latest/css/shieldui-all.min.css">\n    <link rel="stylesheet" href="http://www.shieldui.com/shared/components/latest/css/light-bootstrap/all.min.css">\n    <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/dark-bootstrap/all.min.css">\n    ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'headlinks'):
             context['self'].headlinks(**pageargs)
         
@@ -43,7 +43,7 @@ def render_body(context,**pageargs):
         __M_writer('\n\n    <!-- Latest compiled and minified JavaScript -->\n ')
         __M_writer(str( static_renderer.get_template_css(request, context)  ))
         __M_writer('\n    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>\n    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>\n    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>\n    <meta name="viewport" content="width=device-width, initial-scale=1">\n')
-        __M_writer('   \n  \n  </head>\n  <body>\n    ')
+        __M_writer('    \n  \n  </head>\n  <body>\n    ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'content'):
             context['self'].content(**pageargs)
         
@@ -73,18 +73,6 @@ def render_content(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
-def render_footlinks(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def footlinks():
-            return render_footlinks(context)
-        __M_writer = context.writer()
-        __M_writer('\n\n    ')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
 def render_headlinks(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
@@ -97,8 +85,20 @@ def render_headlinks(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_footlinks(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def footlinks():
+            return render_footlinks(context)
+        __M_writer = context.writer()
+        __M_writer('\n\n    ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 """
 __M_BEGIN_METADATA
-{"filename": "/Users/rodneycox/chf/homepage/templates/base.htm", "uri": "base.htm", "source_encoding": "ascii", "line_map": {"64": 37, "70": 37, "76": 43, "16": 4, "82": 43, "88": 22, "94": 22, "31": 2, "32": 4, "33": 5, "18": 0, "100": 94, "37": 5, "38": 15, "43": 24, "44": 27, "45": 27, "46": 33, "51": 39, "56": 45, "57": 46, "58": 46}}
+{"source_encoding": "ascii", "line_map": {"64": 38, "70": 38, "76": 23, "16": 4, "82": 23, "88": 44, "94": 44, "31": 2, "32": 4, "33": 5, "18": 0, "100": 94, "37": 5, "38": 15, "43": 25, "44": 28, "45": 28, "46": 34, "51": 40, "56": 46, "57": 47, "58": 47}, "uri": "base.htm", "filename": "/Users/rodneycox/chf/homepage/templates/base.htm"}
 __M_END_METADATA
 """
--- homepage/cached_templates/templates/users.html.py
+++ homepage/cached_templates/templates/users.html.py
@@ -4,13 +4,13 @@ UNDEFINED = runtime.UNDEFINED
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1422761508.182613
+_modified_time = 1422773375.335492
 _enable_loop = True
 _template_filename = '/Users/rodneycox/chf/homepage/templates/users.html'
 _template_uri = 'users.html'
 _source_encoding = 'ascii'
 import os, os.path, re
-_exports = ['content', 'footlinks', 'headlinks']
+_exports = ['content', 'headlinks', 'footlinks']
 
 
 def _mako_get_namespace(context, name):
@@ -30,10 +30,10 @@ def render_body(context,**pageargs):
         __M_locals = __M_dict_builtin(pageargs=pageargs)
         def content():
             return render_content(context._locals(__M_locals))
-        def footlinks():
-            return render_footlinks(context._locals(__M_locals))
         def headlinks():
             return render_headlinks(context._locals(__M_locals))
+        def footlinks():
+            return render_footlinks(context._locals(__M_locals))
         __M_writer = context.writer()
         __M_writer('\n  \n  ')
         if 'parent' not in context._data or not hasattr(context._data['parent'], 'headlinks'):
@@ -62,19 +62,7 @@ def render_content(context,**pageargs):
         def content():
             return render_content(context)
         __M_writer = context.writer()
-        __M_writer('\n  <div id="wrapper">\n\n    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">\n        <div class="navbar-header">\n            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">\n                <span class="sr-only">Toggle navigation</span>\n                <span class="icon-bar"></span>\n                <span class="icon-bar"></span>\n                <span class="icon-bar"></span>\n            </button>\n            <a class="navbar-brand" href="index.html">Admin Panel</a>\n        </div>\n\n        <div class="collapse navbar-collapse navbar-ex1-collapse">\n            <ul id="active" class="nav navbar-nav side-nav">\n                <li class="selected"><a href="users.html"><i class="fa fa-bullseye"></i> Users</a>\n                </li>\n                <li><a href="#"><i class="fa fa-tasks"></i> Events</a>\n                </li>\n                <li><a href="#"><i class="fa fa-globe"></i></a>\n                </li>\n                <li><a href="#"><i class="fa fa-list-ol"></i> Rental Items</a>\n                </li>\n                <li><a href="#"><i class="fa fa-font"></i> Products</a>\n                </li>\n                <li><a href="#"><i class="fa fa-font"></i> Timeline</a>\n                </li>\n                <li><a href="#"><i class="fa fa-list-ol"></i> Forms</a>\n                </li>\n                <li><a href="#"><i class="fa fa-font"></i> Typography</a>\n                </li>\n                <li><a href="#"><i class="fa fa-list-ul"></i> Bootstrap Elements</a>\n                </li>\n                <li><a href="#"><i class="fa fa-table"></i> Bootstrap Grid</a>\n                </li>\n            </ul>\n\n            <ul class="nav navbar-nav navbar-right navbar-user">\n              \n                <li class="dropdown user-dropdown">\n                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Rodney Cox<b class="caret"></b></a>\n                    <ul class="dropdown-menu">\n                        <li><a href="#"><i class="fa fa-user"></i> Profile</a>\n                        </li>\n                        <li><a href="#"><i class="fa fa-gear"></i> Settings</a>\n                        </li>\n                        <li class="divider"></li>\n                        <li><a href="#"><i class="fa fa-power-off"></i> Log Out</a>\n                        </li>\n\n                    </ul>\n                </li>\n                <li class="divider-vertical"></li>\n                           </ul>\n        </div>\n    </nav>\n\n    <div id="page-wrapper">\n\n        <div class="row">\n            <div class="col-lg-12">\n                <h1>Dashboard <small>Statistics and more</small></h1>\n                \n            </div>\n        </div>\n\n    </div>\n\n</div>\n  ')
-        return ''
-    finally:
-        context.caller_stack._pop_frame()
-
-
-def render_footlinks(context,**pageargs):
-    __M_caller = context.caller_stack._push_frame()
-    try:
-        def footlinks():
-            return render_footlinks(context)
-        __M_writer = context.writer()
-        __M_writer('\n    <script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>\n    <script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script>\n  ')
+        __M_writer('\n  <div id="wrapper">\n\n    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">\n        <div class="navbar-header">\n            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">\n                <span class="sr-only">Toggle navigation</span>\n                <span class="icon-bar"></span>\n                <span class="icon-bar"></span>\n                <span class="icon-bar"></span>\n            </button>\n            <a class="navbar-brand" href="index.html"><img class="nav-logo" src="/static/homepage/media/chf-logo.svg"></a>\n        </div>\n\n        <div class="collapse navbar-collapse navbar-ex1-collapse">\n            <ul id="active" class="nav navbar-nav side-nav">\n                <li class="selected"><a href="users.html"><i class="fa fa-users"></i>  Users</a>\n                </li>\n                <li><a href="#"><i class="fa fa-map-marker"></i>  Locations</a>\n                </li>\n                <li><a href="#"><i class="fa fa-calendar-o"></i>  Events</a>\n                </li>\n                <li><a href="#"><i class="fa fa-list-ol"></i>  Rental Items</a>\n                </li>\n                <li><a href="#"><i class="fa fa-money"></i>  Products </a><div class="arrow-right"></div>\n                </li>\n            </ul>\n\n            <ul class="nav navbar-nav navbar-right navbar-user">\n              \n                <li class="dropdown user-dropdown">\n                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Rodney Cox<b class="caret"></b></a>\n                    <ul class="dropdown-menu">\n                        <li><a href="#"><i class="fa fa-user"></i> Profile</a>\n                        </li>\n                        <li><a href="#"><i class="fa fa-gear"></i> Settings</a>\n                        </li>\n                        <li class="divider"></li>\n                        <li><a href="#"><i class="fa fa-power-off"></i> Log Out</a>\n                        </li>\n\n                    </ul>\n                </li>\n                <li class="divider-vertical"></li>\n                           </ul>\n        </div>\n    </nav>\n\n    <div id="page-wrapper">\n\n        <div class="row">\n            <div class="col-lg-12">\n                <h1>Dashboard <small>Statistics and more</small></h1>\n                \n            </div>\n        </div>\n\n    </div>\n\n</div>\n  ')
         return ''
     finally:
         context.caller_stack._pop_frame()
@@ -92,8 +80,20 @@ def render_headlinks(context,**pageargs):
         context.caller_stack._pop_frame()
 
 
+def render_footlinks(context,**pageargs):
+    __M_caller = context.caller_stack._push_frame()
+    try:
+        def footlinks():
+            return render_footlinks(context)
+        __M_writer = context.writer()
+        __M_writer('\n    <script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>\n    <script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script>\n  ')
+        return ''
+    finally:
+        context.caller_stack._pop_frame()
+
+
 """
 __M_BEGIN_METADATA
-{"filename": "/Users/rodneycox/chf/homepage/templates/users.html", "uri": "users.html", "source_encoding": "ascii", "line_map": {"65": 7, "27": 0, "38": 1, "71": 79, "43": 5, "77": 79, "48": 77, "83": 3, "53": 82, "89": 3, "59": 7, "95": 89}}
+{"source_encoding": "ascii", "line_map": {"65": 7, "27": 0, "38": 1, "71": 3, "43": 5, "77": 3, "48": 67, "83": 69, "53": 72, "89": 69, "59": 7, "95": 89}, "uri": "users.html", "filename": "/Users/rodneycox/chf/homepage/templates/users.html"}
 __M_END_METADATA
 """
